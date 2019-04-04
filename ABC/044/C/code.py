a75e1db390c387c01df8927c506716c62003b988
ShunKOZAWA <crewfanq@gmail.com>
2019-04-04 22:36:44 +0900

    044,045 C回答追加

--- ABC/044/C/code.py
+++ ABC/044/C/code.py
@@ -14,21 +14,19 @@ def main():
 
     # dp[n][k][s] : n枚目までからk枚選んで合計をsにする場合の数
     dp = [[[0 for _ in range(2501)] for _ in range(N+1)] for _ in range(N+1)]
+
     dp[0][0][0] = 1
-    
     for n in range(N+1):
         for k in range(n+1):
-            for s in range(50*k+1):
-                if(n>=1 and s<X[n-1]):
+            for s in range(2501):            
+                if(n>=1 and X[n-1] > s):
                     dp[n][k][s] = dp[n-1][k][s]
-                if(n>=1 and s>=X[n-1]):
+                elif(n>=1 and X[n-1] <= s):
                     dp[n][k][s] = dp[n-1][k][s] + dp[n-1][k-1][s-X[n-1]]
-
-    Ans = 0
-    for i in range(1,N+1):
-        Ans += dp[N][i][i*A]
- 
-    print(Ans)
-    
+                print(n,k,s,dp[n][k][s])
+    ans = 0
+    for k in range(1,N+1):
+        ans = ans + dp[N][k][k*A]
+    print(ans)
 if __name__ == '__main__':
     main()
