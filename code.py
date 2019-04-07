3038283be5362129963b342c2a6a64402feb0530
ShunKOZAWA <crewfanq@gmail.com>
2019-04-07 22:00:31 +0900

    テンプレートに切り上げ、切り下げを追加

--- code.py
+++ code.py
@@ -1,35 +1,38 @@
 from functools import reduce
 
 def main():
+    # 切り捨て
+    # 4 // 3
+    # 切り上げ
+    #-(-4 // 3)
+    
     # 初期値用:十分大きい数(100億)
     # 1e10
 
     # 初期値用:十分小さい数(-100億)
     # -1e10
     
-    # 一文字のみを読み込み
-    # s = input().rstrip().split(' ')
+    # 1文字のみを読み込み
+    # 入力:2
+    a = input().rstrip()
+    # 変数:a='2'
     
     # スペース区切りで標準入力を配列として読み込み
-    # s = input().rstrip().split(' ')
-
-    # 位置文字ずつ標準入力を配列として読み込み
-    # s = list(input().rstrip())
+    # 入力:2 4 5 7
+    # a, b, c, d = (int(_) for _ in input().split())  
+    # 変数:a=2 b=4 c=5 d =7
     
-    N = int(input().rstrip())
-    a_list = input().rstrip().split(' ')
-    cost = 0
-    min_cost = 1e10
-    for i in range(-100,101):
-        cost = 0
-        for a in a_list:
-            a_num = int(a)
-            cost = cost + (a_num - i) * (a_num-i)
-            # print(i,a_num,cost)
-        if(cost < min_cost):
-            min_cost = cost
-            # print('new ans',min_cost)
-    print(min_cost)
+    # 1文字ずつ標準入力を配列として読み込み
+    # 入力:2 4 5 7
+    # a = list(int(_) for _ in input().split())
+    # 変数:a = [2, 4, 5, 7]    
+
+    # 1文字ずつ標準入力を配列として読み込み
+    # 入力:2457
+    # a = list(int(_) for _ in input())
+    # 変数:a = [2, 4, 5, 7]    
+    
+    print(a)
     
 if __name__ == '__main__':
     main()
