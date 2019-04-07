6a2e42493a1ae898449eef6d493d2551f0596edb
ShunKOZAWA <crewfanq@gmail.com>
2019-04-07 22:00:50 +0900

    ABC/048を追加

---
+++ ABC/048/D/code.py
@@ -0,0 +1,48 @@
+from functools import reduce
+
+def main():
+    # 切り捨て
+    # 4 // 3
+    # 切り上げ
+    #-(-4 // 3)
+    
+    # 初期値用:十分大きい数(100億)
+    # 1e10
+
+    # 初期値用:十分小さい数(-100億)
+    # -1e10
+    
+    # 1文字のみを読み込み
+    # 入力:2
+    # a = input().rstrip()
+    # 変数:a='2'
+    
+    # スペース区切りで標準入力を配列として読み込み
+    # 入力:2 4 5 7
+    # a, b, c, d = (int(_) for _ in input().split())  
+    # 変数:a=2 b=4 c=5 d =7
+    
+    # 1文字ずつ標準入力を配列として読み込み
+    # 入力:2 4 5 7
+    # a = list(int(_) for _ in input().split())
+    # 変数:a = [2, 4, 5, 7]    
+
+    # 1文字ずつ標準入力を配列として読み込み
+    # 入力:2457
+    # a = list(int(_) for _ in input())
+    # 変数:a = [2, 4, 5, 7]    
+    s = list(_ for _ in input())
+
+    if(s[0] == s[-1]):
+        if(len(s) % 2 == 0):
+            print('First')
+        else:
+            print('Second')
+    else:
+        if(len(s) % 2 == 0):
+            print('Second')
+        else:
+            print('First')
+            
+if __name__ == '__main__':
+    main()
