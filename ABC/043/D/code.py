640659f1c9ee65ea70c5076ff02fe9161782eebd
ShunKOZAWA <crewfanq@gmail.com>
2019-04-21 23:16:00 +0900

    ABC/043/Dを追加

--- ABC/043/D/code.py
+++ ABC/043/D/code.py
@@ -1,30 +1,60 @@
 from functools import reduce
+import math
 
 def main():
-    # 一文字のみを読み込み
-    # s = input().rstrip().split(' ')
+    # 文字列の2進数を数値にする
+    # '101' → '5'
+    # 文字列の頭に'0b'をつけてint()にわたす
+    # binary = int('0b'+'101',0)
+
+    # 2進数で立っているbitを数える
+    # 101(0x5) → 2
+    # cnt_bit = bin(5).count('1')
+    
+    # N! を求める
+    # f = math.factorial(N)
+    
+    # 切り捨て
+    # 4 // 3
+    # 切り上げ
+    #-(-4 // 3)
+    
+    # 初期値用:十分大きい数(100億)
+    INF = float("inf")
+    
+    # 1文字のみを読み込み
+    # 入力:2
+    # a = input().rstrip()
+    # 変数:a='2'
     
     # スペース区切りで標準入力を配列として読み込み
-    # s = input().rstrip().split(' ')
-
-    # 位置文字ずつ標準入力を配列として読み込み
-    # s = list(input().rstrip())
-    n = int(input().rstrip())
-    slist = input().rstrip().split(' ')
-    num_slist = []
-    Alice = 0
-    Bob = 0
-    for i in range(0,n):
-        num_slist.append(int(slist[i]))
-    num_slist.sort(reverse = True)
+    # 入力:2 4 5 7
+    # a, b, c, d = (int(_) for _ in input().split())  
+    # 変数:a=2 b=4 c=5 d =7
     
-    for i in range(0,n):
-        if i % 2 == 0:
-            Alice = Alice + int(num_slist[i])
-        else:
-            Bob = Bob + int(num_slist[i])
+    # 1文字ずつ標準入力を配列として読み込み
+    # 入力:2 4 5 7
+    # a = list(int(_) for _ in input().split())
+    # 変数:a = [2, 4, 5, 7]    
+
+    # 1文字ずつ標準入力を配列として読み込み
+    # 入力:2457
+    # a = list(int(_) for _ in input())
+    # 変数:a = [2, 4, 5, 7]    
+    s = list(_ for _ in input())
+    left = -1
+    right = -1
+    for i in range(len(s)-1):
+        if(s[i+1]==s[i]):
+            left = i+1
+            right = i+2
+            break
         
-    print(Alice - Bob)    
-   
+    for i in range(len(s)-2):
+        if(s[i+2]==s[i]):
+            left = i+1
+            right = i+3
+            break
+    print(left,right)
 if __name__ == '__main__':
     main()
