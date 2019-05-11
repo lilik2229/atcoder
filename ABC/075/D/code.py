1764d48e898416b7237c1e2fb6c880a99abf8fc4
ShunKOZAWA <crewfanq@gmail.com>
2019-05-11 15:45:58 +0900

    ABC/075/Dを追加

--- ABC/075/D/code.py
+++ ABC/075/D/code.py
@@ -33,7 +33,7 @@ def main():
     
     # 1文字のみを読み込み
     # 入力:2
-    a = input().rstrip()
+    # a = input().rstrip()
     # 変数:a='2'
     
     # スペース区切りで標準入力を配列として読み込み
@@ -50,8 +50,30 @@ def main():
     # 入力:2457
     # a = list(int(_) for _ in input())
     # 変数:a = [2, 4, 5, 7]    
-    
-    print(a)
+    N, K = (int(_) for _ in input().split())
+    p = []
+    for i in range(N):
+        x,y = (int(_) for _ in input().split())
+        p.append((x,y))
+        
+    ans = INF
+    for i in range(N):
+        for j in range(i+1,N):
+            for h in range(N):
+                for g in range(h+1,N):
+                    x_start = min(p[i][0],p[j][0])
+                    y_start = min(p[h][1],p[g][1])
+                    x_end = max(p[i][0],p[j][0])
+                    y_end = max(p[h][1],p[g][1])
+                    area = (x_end - x_start) * (y_end - y_start)
+                    cnt = 0
+                    for k in range(N):
+                        if p[k][0] >= x_start and p[k][1] >= y_start and p[k][0] <= x_end and p[k][1] <= y_end:
+                            cnt += 1
+                            if cnt == K:
+                                ans = min(ans,area)
+                                break
+    print(ans)
     
 if __name__ == '__main__':
     main()
