d7b5adbe580d365ea8e112a5a34f107b64a12f23
ShunKOZAWA <crewfanq@gmail.com>
2019-05-25 23:30:11 +0900

    ABC/084/Bを追加

--- ABC/084/B/code.py
+++ ABC/084/B/code.py
@@ -33,7 +33,7 @@ def main():
     
     # 1文字のみを読み込み
     # 入力:2
-    a = input().rstrip()
+    # a = input().rstrip()
     # 変数:a='2'
     
     # スペース区切りで標準入力を配列として読み込み
@@ -50,8 +50,16 @@ def main():
     # 入力:2457
     # a = list(int(_) for _ in input())
     # 変数:a = [2, 4, 5, 7]    
-    
-    print(a)
-    
+    A, B = (int(_) for _ in input().split())
+    S = input()
+    if S.count('-') != 1:
+        print('No')
+        return
+    before_str, after_str = S.split('-')
+    if len(before_str) == A and len(after_str)  ==B:
+        print('Yes')
+    else:
+        print('No')
+        
 if __name__ == '__main__':
     main()
