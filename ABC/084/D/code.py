cf931475e71bec73c0ff00b92e30e5b0b9875c84
ShunKOZAWA <crewfanq@gmail.com>
2019-05-27 23:11:00 +0900

    ABC/084/Dを追加

--- ABC/084/D/code.py
+++ ABC/084/D/code.py
@@ -1,6 +1,17 @@
 from functools import reduce
 import math
-
+import collections
+## エラトステネスの篩
+def eratosthenes(num):
+    is_prime = [1]*num
+    is_prime[0] = 0
+    is_prime[1] = 0
+    for i in range(2,num):
+        if is_prime[i] == 1:
+            for j in range(i*i,num,i):
+                is_prime[j] = 0
+    return is_prime
+            
 def main():
     # 文字列の2進数を数値にする
     # '101' → '5'
@@ -33,7 +44,7 @@ def main():
     
     # 1文字のみを読み込み
     # 入力:2
-    a = input().rstrip()
+    # a = input().rstrip()
     # 変数:a='2'
     
     # スペース区切りで標準入力を配列として読み込み
@@ -50,8 +61,28 @@ def main():
     # 入力:2457
     # a = list(int(_) for _ in input())
     # 変数:a = [2, 4, 5, 7]    
-    
-    print(a)
+    Q = int(input())
+    l = []
+    r = []
+    for i in range(Q):
+        s1, s2 = (int(_) for _ in input().split())
+        l.append(s1)
+        r.append(s2)
+    cnt = 1
+    is_prime = eratosthenes(100005)
+
+    c = [0]*100005
+    for i in range(3,100005,2):
+        if is_prime[i] ==1 and is_prime[(i+1)//2] ==1:
+            c[i] = 1
+
+    for i in range(1,100004):
+        c[i+1] += c[i]
+    ans = []    
+    for i in range(Q):
+        ans.append(c[r[i]]-c[l[i]-1])
+            
+    print('\n'.join(str(_) for _ in ans))
     
 if __name__ == '__main__':
     main()
