f9d56cf66511d0bc43da2647689fe8d2826b2c74
ShunKOZAWA <crewfanq@gmail.com>
2019-07-12 23:11:20 +0900

    ABC/098/Cを追加

--- ABC/098/C/code.py
+++ ABC/098/C/code.py
@@ -33,7 +33,7 @@ def main():
     
     # 1文字のみを読み込み
     # 入力:2
-    a = input().rstrip()
+    # a = input().rstrip()
     # 変数:a='2'
     
     # スペース区切りで標準入力を配列として読み込み
@@ -50,8 +50,22 @@ def main():
     # 入力:2457
     # a = list(int(_) for _ in input())
     # 変数:a = [2, 4, 5, 7]    
-    
-    print(a)
+    N = int(input())
+    S = list(_ for _ in input())
+    l = S.count('E')
+    r = 0
+    ans = min(S.count('E'),S.count('W'))
+    for i in range(N):
+        if i != N-1:
+            if S[i] == 'E':
+                l = l - 1
+        if i != 0:                        
+            if prev == 'W':
+                r = r + 1
+        prev = S[i]
+        ans = min(ans,l+r)
+        # print(ans,l,r)
+    print(ans)
     
 if __name__ == '__main__':
     main()
