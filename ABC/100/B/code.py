ec1b7c478ed219ceba4839a7ab5af806f5e9d879
ShunKOZAWA <crewfanq@gmail.com>
2019-07-15 22:00:58 +0900

    ABC/099/Cを追加

--- ABC/100/B/code.py
+++ ABC/100/B/code.py
@@ -33,7 +33,7 @@ def main():
     
     # 1文字のみを読み込み
     # 入力:2
-    a = input().rstrip()
+    # a = input().rstrip()
     # 変数:a='2'
     
     # スペース区切りで標準入力を配列として読み込み
@@ -50,8 +50,11 @@ def main():
     # 入力:2457
     # a = list(int(_) for _ in input())
     # 変数:a = [2, 4, 5, 7]    
-    
-    print(a)
-    
+    D, N = (int(_) for _ in input().split())
+    if N!=100:
+        print(100**D * N)
+    else:
+        print(100**D * N + 1*(100**D))
+        
 if __name__ == '__main__':
     main()
