309fd706133375b887dcbd741291974921643ddf
ShunKOZAWA <crewfanq@gmail.com>
2019-07-19 23:27:12 +0900

    ABC/101/Cを追加

--- ABC/101/B/code.py
+++ ABC/101/B/code.py
@@ -33,7 +33,7 @@ def main():
     
     # 1文字のみを読み込み
     # 入力:2
-    a = input().rstrip()
+    # a = input().rstrip()
     # 変数:a='2'
     
     # スペース区切りで標準入力を配列として読み込み
@@ -49,9 +49,13 @@ def main():
     # 1文字ずつ標準入力を配列として読み込み
     # 入力:2457
     # a = list(int(_) for _ in input())
-    # 変数:a = [2, 4, 5, 7]    
-    
-    print(a)
-    
+    # 変数:a = [2, 4, 5, 7]
+    N = int(input())
+    a = list(int(_) for _ in str(N))
+    if N%sum(a) ==0:
+        print('Yes')
+    else:
+        print('No')
+        
 if __name__ == '__main__':
     main()
